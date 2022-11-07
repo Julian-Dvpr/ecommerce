0fd80a802c14cbb53e8b6e6ede5bf0f7536bbf36
Julian Dvpr <87943487+Julian-Dvpr@users.noreply.github.com>
2022-11-06 21:47:18 -0300

    comando start añadido 3

--- server.js
+++ server.js
@@ -46,8 +46,12 @@ mongoose.connect(URI, {
     console.log("Connected to mongoDB");
 })
 
-
-
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
