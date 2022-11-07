68a5b27b356d915484486e5c6723fc9105b699f9
Julian Dvpr <87943487+Julian-Dvpr@users.noreply.github.com>
2022-11-06 22:06:50 -0300

    kitch

--- server.js
+++ server.js
@@ -49,9 +49,10 @@ mongoose.connect(URI, {
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"))
     app.get("*", (req, res)=>{
-        res.sendFile(path.join(__dirname, "client/build", "index.html"))
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"))
     })
 }
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
