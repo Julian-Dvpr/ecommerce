743ca9a956378785a9626cfd2c3db1221d9741ed
Julian Dvpr <87943487+Julian-Dvpr@users.noreply.github.com>
2022-12-25 14:13:49 -0300

    cambios index server

--- client/src/components/mainpages/createProduct/Createproduct.js
+++ client/src/components/mainpages/createProduct/Createproduct.js
@@ -130,8 +130,8 @@ function Createproduct() {
             <div className="upload">
                 <input type="file" name="file" id="file_up" onChange={handleUpload} />
                 {
-                    loading ? <div id="file_img"><Loading/></div>
-
+                    loading 
+                    ? <div id="file_img"><Loading/></div>
                     :<div id="file_img" style={styleUpload}>
                         <span onClick={handleDestroy}> X </span>
                         <img src={images ? images.url : ""} alt="" />
