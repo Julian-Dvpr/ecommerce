12a142de2be3af9d73493c1fbf6a6916e0fcf769
Julian Dvpr <87943487+Julian-Dvpr@users.noreply.github.com>
2022-12-26 09:09:28 -0300

    ecommerce-kitch

--- server.js
+++ server.js
@@ -1,50 +1,56 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const fileUpload = require("express-fileupload")
-const cookieParser = require("cookie-parser")
-const path = require("path")
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:5000",
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
-
-
-const app = express()
-var corsOptions= {
-    origin:"http://localhost:5000",
-    optionsSuccessStatus:200,
-    credentials:true
-    
-}
-
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors(corsOptions))
-app.use(fileUpload({
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use(
+  fileUpload({
     useTempFiles: true,
-}))
-
+  })
+);
 
 //Routes
 
-app.use("/user", require("./routes/userRouter"))
-app.use("/api", require("./routes/categoryRouter"))
-app.use("/api", require("./routes/upload"))
-app.use("/api", require("./routes/productRouter"))
-app.use("/api", require("./routes/paymentRouter"))
-
-
-
+app.use("/user", require("./routes/userRouter"));
+app.use("/api", require("./routes/categoryRouter"));
+app.use("/api", require("./routes/upload"));
+app.use("/api", require("./routes/productRouter"));
+app.use("/api", require("./routes/paymentRouter"));
 
 //connect to mongodb
 
-const URI = process.env.MONGODB_URL
-mongoose.connect(URI, {
+const URI = process.env.MONGODB_URL;
+mongoose.connect(URI, {}, (err) => {
+  if (err) throw err;
+  console.log("Connected to mongoDB");
+});
 
-}, err=>{
-    if(err) throw err
-    console.log("Connected to mongoDB");
-})
+
+//vercel config
+/* app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+}); */
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"))
@@ -54,7 +60,7 @@ if(process.env.NODE_ENV === "production"){
 }
 
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
-    console.log("server is running", PORT);
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("server is running", PORT);
+});
